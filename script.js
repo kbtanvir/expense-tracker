7927eccecb88108e9636067542d31c4e5747b30b
kbtanvir <tanvirkhaan004@gmail.com>
2024-06-06 18:17:21 +0600

    moved tailwind script to header to preload styles

--- script.js
+++ script.js
@@ -8,9 +8,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("transaction-form");
   const textInput = document.getElementById("transaction-text");
   const amountInput = document.getElementById("transaction-amount");
-  const toggleDarkModeBtn = document.getElementById("toggle-dark-mode");
 
-  let transactions = [];
+  const transactions = [];
 
   // Event listener for form submission
   form.addEventListener("submit", e => {
@@ -28,70 +27,13 @@ document.addEventListener("DOMContentLoaded", () => {
     amountInput.value = "";
   });
 
-  // !dark mode start
-
-  if (
-    localStorage.getItem("color-theme") === "dark" ||
-    (!("color-theme" in localStorage) &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches)
-  ) {
-    document.documentElement.classList.add("dark");
-  } else {
-    document.documentElement.classList.remove("dark");
-  }
-
-  var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
-  var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
-
-  // Change the icons inside the button based on previous settings
-
-  if (
-    localStorage.getItem("color-theme") === "dark" ||
-    (!("color-theme" in localStorage) &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches)
-  ) {
-    themeToggleLightIcon.classList.remove("hidden");
-  } else {
-    themeToggleDarkIcon.classList.remove("hidden");
-  }
-
-  var themeToggleBtn = document.getElementById("theme-toggle");
-
-  themeToggleBtn.addEventListener("click", function () {
-    // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle("hidden");
-    themeToggleLightIcon.classList.toggle("hidden");
-
-    // if set via local storage previously
-    if (localStorage.getItem("color-theme")) {
-      if (localStorage.getItem("color-theme") === "light") {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("color-theme", "dark");
-      } else {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("color-theme", "light");
-      }
-
-      // if NOT set via local storage previously
-    } else {
-      if (document.documentElement.classList.contains("dark")) {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("color-theme", "light");
-      } else {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("color-theme", "dark");
-      }
-    }
-  });
-
-  // !dark mode end
-
   // add a transaction
 
   function addTransaction(text, amount) {
     const transaction = { id: Date.now(), text, amount };
+    
     if (Math.abs(amount) == 0) {
-      alert("Why adding 0?");
+      alert("0 is not allowed?");
       return;
     }
 
@@ -105,6 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     transactions.push(transaction);
+    
     updateUI();
   }
 
@@ -144,16 +87,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const totalIncome = calculateTotalIncome();
     const totalExpenses = calculateTotalExpenses();
 
-    if (totalBalance < totalExpenses) {
-      alert(
-        "Total balance is less than expenses. Cannot add this transaction."
-      );
-      // Remove the last added transaction
-
-      transactions.pop();
-      return;
-    }
-
     balanceElement.textContent = `$${totalBalance.toFixed(2)}`;
     incomeElement.textContent = `$${totalIncome.toFixed(2)}`;
     expenseElement.textContent = `$${Math.abs(totalExpenses).toFixed(2)}`;
@@ -171,4 +104,64 @@ document.addEventListener("DOMContentLoaded", () => {
       })
       .join("");
   }
+
+  // !dark mode start
+
+  if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+
+  const themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+  const themeToggleLightIcon = document.getElementById(
+    "theme-toggle-light-icon"
+  );
+
+  // Change the icons inside the button based on previous settings
+
+  if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    themeToggleLightIcon.classList.remove("hidden");
+  } else {
+    themeToggleDarkIcon.classList.remove("hidden");
+  }
+
+  const themeToggleBtn = document.getElementById("theme-toggle");
+
+  themeToggleBtn.addEventListener("click", function () {
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle("hidden");
+    themeToggleLightIcon.classList.toggle("hidden");
+
+    // if set via local storage previously
+    if (localStorage.getItem("color-theme")) {
+      if (localStorage.getItem("color-theme") === "light") {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("color-theme", "light");
+      }
+
+      // if NOT set via local storage previously
+    } else {
+      if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("color-theme", "light");
+      } else {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+      }
+    }
+  });
+
+  // !dark mode end
 });
